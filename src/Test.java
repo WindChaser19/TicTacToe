9b478433a4a36381268a042def36ec528d8024f4
WindChaser <sarahr.xuan@gmail.com>
2022-05-18 20:13:03 -0700

    Test commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Test {
+
+    public static main (String [] args) throws IOException {
+        System.out.println("Test");
+    }
+
+}
